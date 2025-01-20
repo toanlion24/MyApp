aba95a8eb2b3254be0acc3d6041f7bb5cd58b3e6
toanlion24 <87869737+toanlion24@users.noreply.github.com>
2025-01-21 01:08:07 +0700

    oke

---
+++ src/code/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen.js';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/code/screens/LoginScreen.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Xử lý logic đăng nhập ở đây
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Đăng Nhập</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Mai Thanh Toàn "
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="0221066 "
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Đăng Nhập" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
+
+export default LoginScreen;
